57b3fcd65c2d692c8c8e9f4d4f09e332534fb770
ibaltaci <ibaltaci001@gmail.com>
2023-11-02 22:59:42 +0300

    V1.0.1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import { useEffect, useState } from "react";
-import { createWorker } from "tesseract.js";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { createWorker } from 'tesseract.js';
+
 
 function App() {
   const [ocr, setOcr] = useState("");
   const [imageData, setImageData] = useState(null);
-
+  const [imageDataUri, setImageDataUri] = useState("");
   useEffect(() => {
     const convertImageToText = async () => {
       if (!imageData) return;
-      const worker = createWorker({
-        logger: (m) => {
-          console.log(m);
-        },
-      });
-
-      await worker.load();
-      await worker.loadLanguage("eng");
-      await worker.initialize("eng");
-      const { data: { text } } = await worker.recognize(imageData);
-      setOcr(text);
-
+      const worker = await createWorker('eng');
+      const ret = await worker.recognize(imageDataUri);
+      console.log(ret.data.text);
       await worker.terminate();
     };
 
     convertImageToText();
-  }, [imageData]);
+  }, [imageData,imageDataUri]);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -34,7 +25,8 @@ function App() {
     reader.onloadend = () => {
       const imageDataUri = reader.result;
       console.log({ imageDataUri });
-      setImageData(imageDataUri);
+      setImageData(imageDataUri); // imageData'yi güncelleyin
+      setImageDataUri(imageDataUri); // imageDataUri'yi güncelleyin
     };
     reader.readAsDataURL(file);
   };
