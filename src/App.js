e90b5febb9cba1273289488b086b7f473f17482a
ibaltaci <ibaltaci001@gmail.com>
2023-11-02 23:38:07 +0300

    V1.0.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { createWorker } from 'tesseract.js';
-
+import './App.css'
 
 function App() {
   const [ocr, setOcr] = useState("");
   const [imageData, setImageData] = useState(null);
   const [imageDataUri, setImageDataUri] = useState("");
+  const [recognizedText, setRecognizedText] = useState(""); // Yeni bir state ekledik
+  const [lang, setLang] = useState("eng");
+
   useEffect(() => {
     const convertImageToText = async () => {
       if (!imageData) return;
-      const worker = await createWorker('eng');
+      const worker = await createWorker(lang);
       const ret = await worker.recognize(imageDataUri);
-      console.log(ret.data.text);
+      const text = ret.data.text;
+      console.log(text);
+      setRecognizedText(text); // Sonucu state'e kaydettik
       await worker.terminate();
     };
 
     convertImageToText();
-  }, [imageData,imageDataUri]);
+  }, [imageData, imageDataUri,lang]);
+
+  const handleLanguageChange = (e) => {
+    const lang = e.target.value;
+    setLang(lang);
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -25,8 +35,8 @@ function App() {
     reader.onloadend = () => {
       const imageDataUri = reader.result;
       console.log({ imageDataUri });
-      setImageData(imageDataUri); // imageData'yi güncelleyin
-      setImageDataUri(imageDataUri); // imageDataUri'yi güncelleyin
+      setImageData(imageDataUri);
+      setImageDataUri(imageDataUri);
     };
     reader.readAsDataURL(file);
   };
@@ -44,8 +54,24 @@ function App() {
         />
       </div>
       <div className="display-flex">
-        <img src={imageData} alt="" />
-        <p>{ocr}</p>
+        <div>
+          <img src={imageData} alt="" />
+          <p>{ocr}</p>
+        </div>
+        <div>
+          <p>Choose Language:</p>
+          <select value={lang} onChange={handleLanguageChange}>
+            <option value="eng">English</option>
+            <option value="deu">German</option>
+            <option value="fra">French</option>
+            <option value="rus">Russian</option>
+            {/* Diğer dilleri ekleyebilirsiniz */}
+          </select>
+        </div>
+        <div>
+          <p>Recognized Text:</p>
+          <p>{recognizedText}</p> {/* Sonucu görüntülemek için yeni bir <p> ekledik */}
+        </div>
       </div>
     </div>
   );
